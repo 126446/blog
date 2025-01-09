66ed9f3c5ef019fbd3acf78620054d2b8f4b1507
126446 <1226744586@qq.com>
2025-01-09 22:44:36 +0800

    Fix deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+})
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   output: 'export',
   images: {
     unoptimized: true,
   },
   basePath: '/blog', // 因为你的仓库名是 blog
+  // 禁用严格模式以避免开发时的双重渲染
+  reactStrictMode: false,
 }
 
-module.exports = nextConfig 
+module.exports = withMDX(nextConfig) 
